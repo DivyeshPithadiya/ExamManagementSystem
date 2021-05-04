aca628e27230b93076af60b934046819c251fc2d
DivyeshPithadiya <divyeshpithadiya3@gmail.com>
2021-05-05 00:28:22 +0530

    http changed to https

--- changeUsePass.php
+++ changeUsePass.php
@@ -5,7 +5,7 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>	
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.js"></script>
+    <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.js"></script>
     <script type="text/javascript" src="validation.js"> </script>
 
 	</head>
